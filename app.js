c14bde70f9620afc5c800d75cd4014db3175f00b
letsouzza <leticia.sa1907@gmail.com>
2025-02-04 17:26:57 -0300

    Criando Cards- img e textos ok

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ const cidades = [
 
 ]
 
+const container = [
+    {   nome: 'Jandira', 
+        cor: 'orange', 
+        icon: 'ponto-jandira.png', 
+        descricao: 'O Jandira Plaza Shopping é o destino ideal para compras, lazer e gastronomia no coração de Jandira! '
+    },
+    {   nome: 'Barueri', 
+        cor: 'red', 
+        icon: 'ponto-barueri.png', 
+        descricao: 'O Parque Shopping Barueri é o lugar ideal para quem busca conveniência, entretenimento e as melhores opções de compras em um só espaço! '
+    },
+    {   nome: 'Osasco', 
+        cor: 'purple', 
+        icon: 'ponto-osasco.webp',
+        descricao: 'O Shopping União de Osasco é um dos maiores e mais completos da região, reunindo cinema, praça de alimentação e lazer para toda a família. '
+    },
+    {   nome: 'Itapevi', 
+        cor: 'green', 
+        icon: 'ponto-itapevi.jpg',
+        descricao: 'O Ita Shopping Centro é o principal ponto de compras e lazer de Itapevi! '
+    },
+    {   nome: 'Sorocaba', 
+        cor: 'blue', 
+        icon: 'ponto-sorocaba.jpg',
+        descricao: 'O Sorocaba Shopping é o primeiro shopping do interior de São Paulo, inaugurado em 1981.'
+    }
+]
+
 function criarMenu(cidade){
     const novoItem = document.createElement('li')
     const novoLink = document.createElement('a')
@@ -28,7 +56,30 @@ function criarMenu(cidade){
     menu.appendChild(novoItem)
 }
 
+function criarCards(card){
+    const novoBloco = document.createElement('div')
+    const novaImagem = document.createElement('img')
+    const novoTitulo = document.createElement('h1')
+    const novaDescricao = document.createElement('p')
+    const container = document.getElementById('container')
+
+
+    novaImagem.src = `./img/${card.icon}`
+
+    novoTitulo.textContent = card.nome
+
+    novaDescricao.textContent = card.descricao
+
+    novoBloco.appendChild(novaImagem)
+    novoBloco.appendChild(novoTitulo)
+    novoBloco.appendChild(novaDescricao)
+
+    container.appendChild(novoBloco)
+
+}
+
 cidades.forEach(criarMenu)
+container.forEach(criarCards)
 
 
 
